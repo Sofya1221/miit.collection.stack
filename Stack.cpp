a704685ccad6c806095d56e7da90e30df25161eb
Sofya1221 <Sofya1221@yandex.ru>
2021-04-10 22:32:53 +0300

    refactor main code

--- Stack.cpp
+++ Stack.cpp
@@ -1,72 +1,58 @@
-﻿#include <iostream>
-#include <conio.h>
-
-using namespace std;
-
-#include "stack.h"
+﻿#include "Stack.h"
 
 void printMENU() {
-    cout << "\n\n     MENU\n\n";
+    cout << "\n     MENU\n\n";
     cout << "1) Печать стека\n";
     cout << "2) Удалить элемент из стека\n";
-    cout << "3) Текущий размер стека\n";
-    cout << "4) Добавить элемент в стек\n";
-    cout << "5) Получить n-й от вершины жлемент стека\n";
-    cout << "Выход - ESC\n";
-    cout << "\n\n";
+    cout << "3) Добавить элемент в стек\n";
+    cout << "4) Выполнить циклический сдвиг вправо\n";
+    cout << "5) Выполнить циклический сдвиг влево\n";
+    cout << "\nВыход - ESC\n";
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    cout << "Введите число элементов в стеке: ";
-    int ct;
-    cin >> ct;
-    Stack<char> stackSymbol(ct);
-    char ch = NULL;
+    Stack<char> stack;
+    char ch = '0';
     system("cls");
-    while ((int)ch != 27) {
-        system("cls");
-        printMENU();
-        cin >> ch;
+    while (ch != 27) {
+            system("cls");
+            printMENU();
+            ch = _getch();
         switch (ch) {
             case '1': {
                 system("cls");
-                stackSymbol.printStack();
-                _getch();
+                stack.printStack();
+                system("PAUSE");
                 break;
             }
             case '2': {
                 system("cls");
-                stackSymbol.pop();
+                stack.pop();
                 break;
             }
             case '3': {
                 system("cls");
-                stackSymbol.getStackSize();
-                _getch();
+                cout << "Введите новый элемент: ";
+                char symbol;
+                cin >> symbol;
+                stack.push(symbol);
                 break;
             }
             case '4': {
                 system("cls");
-                cout << "Введите новый элемент: ";
-                char symbol;
-                cin >> symbol;
-                stackSymbol.push(symbol);
-                _getch();
+                stack.roll(true);
                 break;
             }
             case '5': {
                 system("cls");
-                cout << "Какой по номеру элемент стека нужен?\n";
-                int number;
-                cin >> number;
-                stackSymbol.Peek(number);
-                _getch();
+                stack.roll(false);
                 break;
             }
         }
     }
-    _getch();
+    system("cls");
+    system("PAUSE");
     return 0;
 }
